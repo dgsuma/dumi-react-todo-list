20b934642a3b74a5a48b6ebb148575fdc96786a4
Duminda <dumindags@gmail.com>
2019-06-06 06:49:04 +0530

    App Component Structure

--- src/App.js
+++ src/App.js
@@ -6,13 +6,51 @@ import TodoList from "./components/TodoList";
 
 // showing vs-code gothub setup
 class App extends Component {
+  state = {
+    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = e => {
+    console.log("handle change");
+  };
+
+  handleSubmit = e => {
+    console.log("handle submit");
+  };
+
+  clearList = () => {
+    console.log("clear list");
+  };
+
+  handleDelete = id => {
+    console.log(`delete ${id}`);
+  };
+
+  handleEdit = id => {
+    console.log(`edit ${id}`);
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <div className="row">
-            <TodoInput />
-            <TodoList />
+      <div className="container">
+        <div className="row">
+          <div className="col-10mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
           </div>
         </div>
       </div>
