bc42334a04290fe5ce11c21eccdd850a2be58bfe
Duminda <dumindags@gmail.com>
2019-06-06 07:37:42 +0530

    add bootstrap classes TodoInput

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,6 +2,32 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
-    return <h1>hello from todo input</h1>;
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-block btn-primary mt-3 text-uppercase"
+          >
+            add item
+          </button>
+        </form>
+      </div>
+    );
   }
 }
