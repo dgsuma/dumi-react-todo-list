e1eb2169f221117a2863136e51ccd6acdb7b5d24
Duminda <dumindags@gmail.com>
2019-06-06 10:19:54 +0530

    edit handleChange & handleSubmit methods in App.js file

--- src/App.js
+++ src/App.js
@@ -7,18 +7,35 @@ import TodoList from "./components/TodoList";
 // showing vs-code gothub setup
 class App extends Component {
   state = {
-    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    items: [],
     id: uuid(),
     item: "",
     editItem: false
   };
 
   handleChange = e => {
-    console.log("handle change");
+    this.setState({
+      item: e.target.value
+    });
   };
 
   handleSubmit = e => {
-    console.log("handle submit");
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+
+    const updatedItems = [...this.state.items, newItem];
+    this.setState(
+      {
+        items: updatedItems,
+        item: "",
+        id: uuid(),
+        editItem: false
+      },
+      () => console.log(this.state)
+    );
   };
 
   clearList = () => {
