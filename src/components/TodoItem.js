5e65c5a75b5e51ad1235aedfb6e1e4b7f5542e27
Duminda <dumindags@gmail.com>
2019-06-08 09:34:19 +0530

    edited TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,10 +8,10 @@ export default class TodoItem extends Component {
         <h6>{title}</h6>
         <div className="todo-icon">
           <span className="mx-2 text-success" onClick={handleEdit}>
-            <i className="fas fa-pen" />{" "}
+            <i className="fas fa-pen" />
           </span>
           <span className="mx-2 text-danger" onClick={handleDelete}>
-            <i className="fas fa-trash" />{" "}
+            <i className="fas fa-trash" />
           </span>
         </div>
       </li>
