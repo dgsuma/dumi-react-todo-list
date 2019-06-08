852e78b5045cfa81d4d2ff038efe062601bc2a67
Duminda <dumindags@gmail.com>
2019-06-08 10:05:35 +0530

    Finished Application and Working order

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
-// showing vs-code github setup
+// Finished Application and Working order
 class App extends Component {
   state = {
     items: [],
